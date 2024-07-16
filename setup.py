5d73004924a7c415033da82ed13353feba507c30
josele <jotasege94@gmail.com>
2024-07-15 21:46:58 -0300

    Change branch

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup    
+
+
+setup(
+    name="segunda_pre_entrega_Giorgio",
+    version="1.0.0",
+    description="Segunda pre entrega de José Giorgio",
+    author="José Giorgio",
+    package=["entrega_finales"]
+)
